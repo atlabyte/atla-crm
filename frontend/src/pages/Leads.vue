e7ef5dc63ef0079e2671611d239031a69df55668
Dastan Nurbekov <dastan.n@wellnutscorp.com>
2026-01-20 13:33:24 +0600

    task/6 fix date status in kanban lead page

--- frontend/src/pages/Leads.vue
+++ frontend/src/pages/Leads.vue
@@ -485,7 +485,7 @@ function parseRows(rows, columns = []) {
         }))
       } else if (['modified', 'creation'].includes(row)) {
         _rows[row] = {
-          label: dayjsLocal(lead[row]).format('DD.MM.YYYY dd'),
+          label: dayjsLocal(lead[row]).format('DD.MM.YYYY hh:mm'),
           timeAgo: __(timeAgo(lead[row])),
         }
       } else if (
